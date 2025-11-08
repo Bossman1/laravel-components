326b520907ae0939e6041bd91d1bb5a394cc7ee8
Bossman1 <nikakharadze@yahoo.com>
2025-11-08 11:04:39 +0400

    update: change structure

--- src/ComponentsServiceProvider.php
+++ src/ComponentsServiceProvider.php
@@ -9,8 +9,24 @@ class ComponentsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Load views
-        $this->loadViewsFrom(__DIR__ . '/views', 'components');
+        // Load package views
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'nickkh');
+
+        // Publish package views for overriding
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/nickkh'),
+        ], 'views');
+
+        // Register Blade components automatically
+        $componentsPath = __DIR__ . '/View/Components';
+        if (is_dir($componentsPath)) {
+            foreach (glob($componentsPath . '/*.php') as $file) {
+                $className = 'NickKh\\Components\\View\\Components\\' . basename($file, '.php');
+                if (class_exists($className)) {
+                    Blade::component($className, strtolower(basename($file, '.php')), 'nickkh');
+                }
+            }
+        }
     }
 
     public function register()
